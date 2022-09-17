ae2a612a26e8cdf3a1be0957f58d1e0f09557e75
Hazim <hazim_malik@hotmail.com>
2022-09-17 18:10:43 +0200

    Uninstall flux

--- flux-install-github/main.tf
+++ flux-install-github/main.tf
@@ -1,107 +1,107 @@
-# Generate manifests
-data "flux_install" "main" {
-  target_path    = var.target_path
-  network_policy = false
-}
-
-resource "kubernetes_namespace" "flux_system" {
-  metadata {
-    name = "flux-system"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels,
-    ]
-  }
-}
-
-# Split multi-doc YAML with
-# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
-data "kubectl_file_documents" "apply" {
-  content = data.flux_install.main.content
-}
-
-# Convert documents list to include parsed yaml data
-locals {
-  apply = [for v in data.kubectl_file_documents.apply.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-# Apply manifests on the cluster
-resource "kubectl_manifest" "apply" {
-  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-data "github_repository" "flux_repo" {
-  full_name = "${var.github_owner}/${var.repository_name}"
-}
-
-resource "github_repository_file" "install" {
-  repository = data.github_repository.flux_repo.name
-  file       = data.flux_install.main.path
-  content    = data.flux_install.main.content
-  branch     = var.branch
-}
-
-# Generate manifests
-data "flux_sync" "main" {
-  target_path = var.target_path
-  url         = data.github_repository.flux_repo.http_clone_url
-}
-
-# Split multi-doc YAML with
-# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
-data "kubectl_file_documents" "sync" {
-  content = data.flux_sync.main.content
-}
-
-# Convert documents list to include parsed yaml data
-locals {
-  sync = [for v in data.kubectl_file_documents.sync.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-# Apply manifests on the cluster
-resource "kubectl_manifest" "sync" {
-  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-# Generate a Kubernetes secret with the Git credentials
-resource "kubernetes_secret" "main" {
-  depends_on = [kubectl_manifest.apply]
-
-  metadata {
-    name      = data.flux_sync.main.secret
-    namespace = data.flux_sync.main.namespace
-  }
-
-  data = {
-    username = "git"
-    password = var.github_token
-  }
-}
-
-resource "github_repository_file" "sync" {
-  repository = data.github_repository.flux_repo.name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "kustomize" {
-  repository = data.github_repository.flux_repo.name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
-}
+//# Generate manifests
+//data "flux_install" "main" {
+//  target_path    = var.target_path
+//  network_policy = false
+//}
+//
+//resource "kubernetes_namespace" "flux_system" {
+//  metadata {
+//    name = "flux-system"
+//  }
+//
+//  lifecycle {
+//    ignore_changes = [
+//      metadata[0].labels,
+//    ]
+//  }
+//}
+//
+//# Split multi-doc YAML with
+//# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+//data "kubectl_file_documents" "apply" {
+//  content = data.flux_install.main.content
+//}
+//
+//# Convert documents list to include parsed yaml data
+//locals {
+//  apply = [for v in data.kubectl_file_documents.apply.documents : {
+//    data : yamldecode(v)
+//    content : v
+//    }
+//  ]
+//}
+//
+//# Apply manifests on the cluster
+//resource "kubectl_manifest" "apply" {
+//  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+//  depends_on = [kubernetes_namespace.flux_system]
+//  yaml_body  = each.value
+//}
+//
+//data "github_repository" "flux_repo" {
+//  full_name = "${var.github_owner}/${var.repository_name}"
+//}
+//
+//resource "github_repository_file" "install" {
+//  repository = data.github_repository.flux_repo.name
+//  file       = data.flux_install.main.path
+//  content    = data.flux_install.main.content
+//  branch     = var.branch
+//}
+//
+//# Generate manifests
+//data "flux_sync" "main" {
+//  target_path = var.target_path
+//  url         = data.github_repository.flux_repo.http_clone_url
+//}
+//
+//# Split multi-doc YAML with
+//# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+//data "kubectl_file_documents" "sync" {
+//  content = data.flux_sync.main.content
+//}
+//
+//# Convert documents list to include parsed yaml data
+//locals {
+//  sync = [for v in data.kubectl_file_documents.sync.documents : {
+//    data : yamldecode(v)
+//    content : v
+//    }
+//  ]
+//}
+//
+//# Apply manifests on the cluster
+//resource "kubectl_manifest" "sync" {
+//  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+//  depends_on = [kubernetes_namespace.flux_system]
+//  yaml_body  = each.value
+//}
+//
+//# Generate a Kubernetes secret with the Git credentials
+//resource "kubernetes_secret" "main" {
+//  depends_on = [kubectl_manifest.apply]
+//
+//  metadata {
+//    name      = data.flux_sync.main.secret
+//    namespace = data.flux_sync.main.namespace
+//  }
+//
+//  data = {
+//    username = "git"
+//    password = var.github_token
+//  }
+//}
+//
+//resource "github_repository_file" "sync" {
+//  repository = data.github_repository.flux_repo.name
+//  file       = data.flux_sync.main.path
+//  content    = data.flux_sync.main.content
+//  branch     = var.branch
+//}
+//
+//resource "github_repository_file" "kustomize" {
+//  repository = data.github_repository.flux_repo.name
+//  file       = data.flux_sync.main.kustomize_path
+//  content    = data.flux_sync.main.kustomize_content
+//  branch     = var.branch
+//}
